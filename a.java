75bbc3aa79b5d03c12d59b6d8d04a9d78f8e1a50
Shubham01kanodia <35887672+Shubham01kanodia@users.noreply.github.com>
2018-02-04 14:13:26 +0530

    Add files via upload

---
+++ a.java
@@ -0,0 +1,18 @@
+class A
+{
+	public void add()
+	{
+		System.out.println("satyam");
+	}
+	public void add(double b)
+	{
+		System.out.println("hiii");
+	}
+	public static void main(String []args)
+	{
+		A a=new A();
+		a.add();
+		a.add(0.0);
+		
+	}
+}
