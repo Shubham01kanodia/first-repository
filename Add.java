62fd46b1e65c227ae5f130b7d11c2951b7bbdeca
Shubham01kanodia <35887672+Shubham01kanodia@users.noreply.github.com>
2018-02-04 15:42:03 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Add
+{
+	static void main(String[]args)
+	{
+		int a,b,c;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter two number");
+		
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=a+b;
+		System.out.println("answer is"+c);
+		
+	}
+	
+}
